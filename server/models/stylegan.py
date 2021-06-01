9b20bf56d387596a835c7cf050cfe410d26a6fee
vipermu <vipermu97@gmail.com>
2021-06-02 00:03:35 +0200

    ch: automatic download of sg

--- server/models/stylegan.py
+++ server/models/stylegan.py
@@ -9,6 +9,11 @@ from PIL import Image
 
 from models.stylegan_models import g_all, g_synthesis, g_mapping
 
+if not os.path.exists('./server/models/'):
+    os.system(
+        "wget 'https://github.com/lernapparat/lernapparat/releases/download/v2019-02-01/karras2019stylegan-ffhq-1024x1024.for_g_all.pt' -O './server/models/sg1.pt'"
+    )
+
 torch.manual_seed(20)
 
 parser = argparse.ArgumentParser()
