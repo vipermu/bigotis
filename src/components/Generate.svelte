adac0d74aab3ec96b86960883a116ef9a98ff77a
vipermu <vipermu97@gmail.com>
2021-06-09 11:01:51 -0500

    style + more interval

--- src/components/Generate.svelte
+++ src/components/Generate.svelte
@@ -1,25 +1,24 @@
 <script>
     import { Jumper } from "svelte-loading-spinners";
-    import {serverIP, apiPort} from "../utils.ts"
-        
-    export let generationParams
-    export let generationResultDict
-    
+    import { serverIP, apiPort } from "../utils.ts";
+
+    export let generationParams;
+    export let generationResultDict;
+
     let generatingImage = false;
 
     async function generate() {
         console.log("GENERATING WITH PARAMS", generationParams);
         generatingImage = true;
 
-        const serverURL = `http://${serverIP}:${apiPort}`
+        const serverURL = `http://${serverIP}:${apiPort}`;
 
         let fetchURL = new URL(`${serverURL}/generate`);
         fetchURL.search = new URLSearchParams(generationParams).toString();
-        let jobId = undefined
-
+        let jobId = undefined;
 
         const getResults = async () => {
-            if (typeof jobId == "undefined"){
+            if (typeof jobId == "undefined") {
                 console.log("FETCHING DATA");
                 const res = await fetch(fetchURL.toString(), {
                     method: "GET",
@@ -27,40 +26,42 @@
 
                 const jobResults = await res.json();
                 console.log("JOB RESULTS", jobResults);
-                jobId = jobResults.jobId
-                
+                jobId = jobResults.jobId;
             } else {
                 try {
-                    console.log("FETCHING RESULTS")
+                    console.log("FETCHING RESULTS");
                     let resultsURL = new URL(`${serverURL}/results/${jobId}`);
-                    console.log("URL", resultsURL.toString())  
+                    console.log("URL", resultsURL.toString());
 
-                    const results = await fetch(resultsURL.toString()).then(res => res.json())
-                    
-                    console.log("RESULTS ", results)
+                    const results = await fetch(resultsURL.toString()).then(
+                        (res) => res.json()
+                    );
+
+                    console.log("RESULTS ", results);
 
                     if (results.finished == "yup") {
                         console.log("DONE!");
-                        generationResultDict = results
-                        console.log("Generation result dict", generationResultDict);               
+                        generationResultDict = results;
+                        console.log(
+                            "Generation result dict",
+                            generationResultDict
+                        );
                         generatingImage = false;
-                        clearInterval(interval)
+                        clearInterval(interval);
                     } else {
                         console.log(results.finished);
                         // setTimeout(getResults, 0)
-
                     }
                 } catch (err) {
                     // catches errors both in fetch and results.json
-                    console.log("ERRROR")
+                    console.log("ERRROR");
                     console.log(err);
                 }
             }
         };
-        
-        let interval = setInterval(getResults, 2000); 
-        // getResults()
 
+        let interval = setInterval(getResults, 5000);
+        // getResults()
     }
 </script>
 
@@ -70,11 +71,6 @@
     </div>
 {:else}
     <div style="margin-top: 10px">
-        <Jumper
-            size="60"
-            color="#BE3CC6"
-            unit="px"
-            duration="1.3s"
-        />
+        <Jumper size="60" color="#BE3CC6" unit="px" duration="1.3s" />
     </div>
 {/if}
